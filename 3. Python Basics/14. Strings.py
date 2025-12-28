8cfc702056699b05227e4899508faf97bbdf7c36
YaseenAminM <yaseenaminm@gmail.com>
2025-12-28 15:00:40 +0500

    14. Strings

---
+++ 3. Python Basics/14. Strings.py
@@ -0,0 +1,25 @@
+# ###################
+# String
+'Hi hello there!'
+"Hi hello there!"
+
+print(type("hi hello there 24!!!"))
+
+user_name = "superCode"
+password = "supersecrete"
+
+
+long_string = '''
+WOW
+---
+OO
+'''
+
+print(long_string)
+
+
+first_name = "Yaseen"
+last_name = "Amin"
+
+full_name = first_name + " " + last_name
+print(full_name)
