41f92f3799585c1ba805580bc238ed8ef7a4900c
YaseenAminM <yaseenaminm@gmail.com>
2025-12-28 14:15:01 +0500

    4. Numbers

---
+++ 3. Python Basics/4. Numbers.py
@@ -0,0 +1,53 @@
+#####################################
+# Fundamental Data Types
+
+#####################################
+# int - integers
+print(2 + 4)
+print(2 - 4)
+print(2 * 4)
+print(2 / 4)
+print(type(2 + 4))  # <class 'int'>
+print(type(2 / 4))  # <class 'float'>
+print(type(0.00001))  # <class 'float'>
+print(type(0))
+
+print(type(2 + 20.00))
+
+#  2 Power of 2 == 2^2
+print(2 ** 2)
+
+print(2//4)
+print(8//4)
+
+print(50 % 5)
+
+#####################################
+# float
+
+
+#####################################
+# bool
+
+#####################################
+# str
+
+#####################################
+# list
+
+#####################################
+# tuple
+
+#####################################
+# set
+
+#####################################
+# dict
+
+
+# Classes -> Custom Types
+# SuperCar
+
+# Specialized Data Types
+# Modules
+# None
