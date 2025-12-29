2a4c54c2b2757cb39817737fbbbe18b95ae69611
Yaseen Amin <yaseenaminm@gmail.com>
2025-12-29 14:34:46 +0500

    19. String Indexes

---
+++ 3. Python Basics/19. String Indexes.py
@@ -0,0 +1,16 @@
+# String Indexes
+
+# "me me me"
+#  01234567 ==> Indexes...
+
+selfish = "me me me"
+print(selfish)
+print(selfish[0])
+
+# [start:end:steps]
+print(selfish[0:2])
+print(selfish[::3])
+print(selfish[1::])
+
+# reverse the string
+print(selfish[::-1])
