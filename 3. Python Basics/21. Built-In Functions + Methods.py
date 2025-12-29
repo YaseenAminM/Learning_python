668d7f698fc216f331cc5a8d36fc054cba6d14d8
Yaseen Amin <yaseenaminm@gmail.com>
2025-12-29 15:35:51 +0500

    21. Built-In Functions + Methods

---
+++ 3. Python Basics/21. Built-In Functions + Methods.py
@@ -0,0 +1,26 @@
+# 21. Built-In Functions + Methods
+
+# str()
+# int()
+# print()
+# len()
+
+# print(len("Hello"))
+
+greet = "Helloooo"
+# print(greet[0:len(greet)])
+
+# Method
+# print(greet.upper())
+
+
+quote = "to be or not to be"
+
+
+print(quote.upper())
+print(quote.capitalize())
+print(quote.lower())
+print(quote.find('be'))
+
+quote2 = quote.replace("be", "me")
+print(quote2)
