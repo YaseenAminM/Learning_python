f8cee4e28f4175f4f2ba492801751074408071aa
Yaseen Amin <yaseenaminm@gmail.com>
2025-12-29 17:11:33 +0500

    26. Lists

---
+++ 3. Python Basics/26. Lists.py
@@ -0,0 +1,13 @@
+# Lists
+li = [1, 2, 3]
+li2 = ['a', 'b', 'c']
+li3 = [1, 2, "a", True]
+
+
+amazon_cart = ["notebooks", "sunglassed "]
+
+print(amazon_cart)
+print(amazon_cart[1])
+print(amazon_cart[-1])
+
+# Data Structures
