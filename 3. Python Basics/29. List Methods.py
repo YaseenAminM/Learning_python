8b5021f7a5bd587f85b21f3ccf873ae43f486c83
Yaseen Amin <yaseenaminm@gmail.com>
2025-12-30 15:05:55 +0500

    29. List Methods

---
+++ 3. Python Basics/29. List Methods.py
@@ -0,0 +1,29 @@
+# List Methods
+
+basket = [1, 2, 3, 4, 5]
+
+# print(len(basket))
+
+# Adding
+# basket.append(6)
+
+# insert
+# basket.insert(1, 200)
+
+# extend
+# basket.extend([100, 101])
+
+# removing
+# basket.pop()
+# basket.pop(0)
+
+# remove a specific value
+# basket.remove(4)
+
+
+# clear whaterver in the list
+# basket.clear()
+
+new_list = basket
+
+print(basket)
