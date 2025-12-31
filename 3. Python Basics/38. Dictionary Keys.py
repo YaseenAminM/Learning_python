eab3da61ce7d9b7ed351342d13484025c436088d
Yaseen Amin <yaseenaminm@gmail.com>
2025-12-31 17:21:18 +0500

    working on dic key...

---
+++ 3. Python Basics/38. Dictionary Keys.py
@@ -0,0 +1,14 @@
+# Dictionary Keys
+dictionary = {
+    123: [1, 2, 3,],
+    'weapons': [1, 2, 3,],
+    "greeting": 'Hello',
+    "is_magic": True,
+    [100]: True
+}
+
+
+print(dictionary[123])
+
+# Can't a element using a array
+# print(dictionary[[100]])  # Error
