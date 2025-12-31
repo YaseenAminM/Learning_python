af9f8b1a3c4fe750cdcf4cd75a19899c5925a9c4
Yaseen Amin <yaseenaminm@gmail.com>
2025-12-31 15:59:24 +0500

    36. 3.7 Update Ordered Dict

---
+++ 3. Python Basics/37. DEVELOPER FUNDAMENTALS III.py
@@ -0,0 +1,23 @@
+# DEVELOPER FUNDAMENTALS III
+# Dictionaries
+
+dictionary = {
+    'a': [1, 2, 3],
+    'b': "hello",
+    'x': True
+}
+
+my_list = [
+    {
+        'a': [1, 2, 3],
+        'b': "hello",
+        'x': True
+    },
+    {
+        'a': [4, 5, 6,],
+        'b': "hello",
+        'x': True
+    }
+]
+# print(dictionary['a'])
+print(my_list[0]['a'])
