9ea9941f2d8fc0f3736b1e4ca97aa7251dbfe173
Yaseen Amin <yaseenaminm@gmail.com>
2026-01-01 16:08:39 +0500

    41. Tuples

---
+++ 3. Python Basics/41. Tuples.py
@@ -0,0 +1,12 @@
+# Tuples
+my_tuple = (1, 2, 3, 4, 5)
+print(my_tuple[1])
+print(5 in my_tuple)
+user = {
+    (1, 2): [1, 2, 3],
+    "basket": [1, 2, 3],
+    "greet": "Hello",
+    "age": 20
+}
+print(user.items())
+print(user[(1, 2)])
