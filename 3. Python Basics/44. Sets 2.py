d4e76594c896bc568af0f0a8aa4c930d8c747927
Yaseen Amin <yaseenaminm@gmail.com>
2026-01-01 17:59:33 +0500

    44. Sets 2

---
+++ 3. Python Basics/44. Sets 2.py
@@ -0,0 +1,32 @@
+# Sets 2
+my_set = {1, 2, 3, 4, 5, 5}
+your_set = {4, 5, 6, 7, 8, 9, 10}
+
+# .difference()
+# .discard()
+# .difference_update()
+# .intersection()
+# .isdisjoint()
+# .issubset()
+# .issuperset()
+# .union()
+
+# print(my_set.difference(your_set))
+# print(my_set.discard(5))
+
+
+# print(my_set.difference_update(your_set))
+# print(my_set)
+
+# print(my_set.intersection(your_set))
+
+# print(my_set.isdisjoint(your_set))
+
+# print(my_set.issubset(your_set))
+# print(your_set.issubset(my_set))
+
+# print(my_set.issuperset(your_set))
+# print(your_set.issuperset(your_set))
+
+
+# print(my_set.union(your_set))
