fa920bed4be16fe76b2374d2ad082bb83dc9b60e
Yaseen Amin <yaseenaminm@gmail.com>
2026-01-01 19:15:04 +0500

    working on indentation...

---
+++ 4. Python Basics 2/3. Indentation In Python.py
@@ -0,0 +1,14 @@
+# Indentation In Python
+
+is_old = True
+is_licenced = True
+
+
+if is_old and is_licenced:
+    print("you are old enough to drive!")
+elif is_licenced:
+    print("you can drive now!")
+else:
+    print("you are not of age!")
+
+print("okok")
