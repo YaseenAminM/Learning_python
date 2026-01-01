af738e24443b78e74ea6b90d49efc25b7415f6ed
Yaseen Amin <yaseenaminm@gmail.com>
2026-01-01 19:10:19 +0500

    2. Conditional Logic

---
+++ 4. Python Basics 2/2. Conditional Logic.py
@@ -0,0 +1,18 @@
+# Conditional Logic
+# True
+# False
+
+is_old = False
+is_licenced = False
+
+# if condition:
+#   ...code
+
+if is_old and is_licenced:
+    print("you are old enough to drive!")
+elif is_licenced:
+    print("you can drive now!")
+else:
+    print("you are not of age!")
+
+print("okok")
