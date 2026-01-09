9141d949ddf6caf1a8d51a0e63fe24954a37677e
Yaseen Amin <yaseenaminm@gmail.com>
2026-01-09 12:32:41 +0500

    5. Ternary Operator

---
+++ 4. Python Basics 2/5. Ternary Operator.py
@@ -0,0 +1,11 @@
+# Ternary Operator
+
+# my_var = a if condition else b
+
+age = 5
+has_licence = "he can drive" if age >= 18 else "he cannot drive"
+# print(has_licence)
+
+is_friend = True
+can_message = "Message allowed" if is_friend else "Message not allowed"
+print(can_message)
