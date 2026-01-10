312ab68c7827e73793d9d0e6429023b590897453
Yaseen Amin <yaseenaminm@gmail.com>
2026-01-10 21:17:04 +0500

    10. For Loops

---
+++ 4. Python Basics 2/10. For Loops.py
@@ -0,0 +1,23 @@
+# For Loops
+
+
+# for item in "Yaseen Amin Maredia":
+#     print(item)
+
+
+# for item in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
+#     print(item)
+
+
+# for item in {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}:
+#     print(item)
+
+
+# for item in (1, 2, 3, 4, 5, 6, 7, 8, 9, 10):
+#     print(item)
+#     print(item)
+#     print(item)
+
+for item in (1, 2, 3, 4):
+    for x in [10, 20, 30, 40]:
+        print(item, x)
