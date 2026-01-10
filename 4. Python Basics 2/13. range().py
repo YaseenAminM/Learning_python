b8cab02c2afe465613adb67e66c0ccbc40757dc8
Yaseen Amin <yaseenaminm@gmail.com>
2026-01-10 21:49:42 +0500

    13. range()

---
+++ 4. Python Basics 2/13. range().py
@@ -0,0 +1,16 @@
+# range()
+
+# print(range(100))
+# print(list(range(100)))
+
+
+# (start,end,step)
+# for number in range(0, 100, -1):
+#     print("Email List : ", number)
+
+
+# reverse loop
+for number in range(10, 0, -1):
+    # print("Email List : ", number)
+    # print(list(range(number)))
+    print("*" * number)
