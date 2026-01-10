d73bc1361e0a4b36b82d9d8985904a6873e9fa05
Yaseen Amin <yaseenaminm@gmail.com>
2026-01-10 20:54:43 +0500

    8. Exercise Logical Operators

--- 4. Python Basics 2/8. Exercise Logical Operators.py
+++ 4. Python Basics 2/8. Exercise Logical Operators.py
@@ -1,1 +1,16 @@
 # Exercise Logical Operators
+
+
+is_magician = False
+is_expert = False
+
+# Chek if magician AND expert : "You are a master magician"
+if is_magician and is_expert:
+    print("You are a master magician")
+
+# Check if magician but not expert: "at least you're getting there"
+
+elif is_expert or (is_expert):
+    print("at least you're getting there")
+elif not is_expert:
+    print("You need magic powers")
