6857142271f2f072a10752dc1fdbc07ea3644e21
Yaseen Amin <yaseenaminm@gmail.com>
2026-01-12 13:59:24 +0500

    17. break, continue, pass

---
+++ 4. Python Basics 2/17. break, continue, pass.py
@@ -0,0 +1,19 @@
+# break, continue, pass
+
+my_list = [1, 2, 3]
+
+for item in my_list:
+    # print(item)
+    # break
+    # continue
+    pass
+    print(item)
+
+i = 0
+while i < len(my_list):
+    # print(my_list[i])
+    print(my_list[i])
+    pass
+    i += 1
+    # break
+    # continue
