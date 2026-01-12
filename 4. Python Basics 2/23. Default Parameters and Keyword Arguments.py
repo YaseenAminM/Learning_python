8aaf2b65ef0c798f580bc468de04686b30adaeb9
Yaseen Amin <yaseenaminm@gmail.com>
2026-01-12 16:35:10 +0500

    Default Parameters and Keyword Arguments

---
+++ 4. Python Basics 2/23. Default Parameters and Keyword Arguments.py
@@ -0,0 +1,13 @@
+# Default Parameters and Keyword Arguments
+
+
+def say_hello(name, emoji):
+    print(f"Hello, {name} {emoji}")
+
+
+# Postional Argument
+say_hello("Yaseen", "👌")
+say_hello("Amin", "😍")
+
+# Keyword Argument
+say_hello(emoji="🤣", name="Noorji")
