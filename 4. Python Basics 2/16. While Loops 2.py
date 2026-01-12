771bd15caf62e7dc3172e493f3b4fc3205d4f0fd
Yaseen Amin <yaseenaminm@gmail.com>
2026-01-12 13:37:37 +0500

    16. While Loops 2

---
+++ 4. Python Basics 2/16. While Loops 2.py
@@ -0,0 +1,25 @@
+# While Loops 2
+# i = 0
+# while i < 50:
+#     print(i)
+#     if i == 21:
+#         print("While Loop end here!")
+#         break
+#     i += 1
+
+my_list = [1, 2, 3]
+
+# for item in my_list:
+#     print(item)
+
+
+# i = 0
+# while i < len(my_list):
+#     print(my_list[i])
+#     i += 1
+
+while True:
+    response = input("Say something?:")
+    if response == 'bye':
+        print("While loop ends here")
+        break
