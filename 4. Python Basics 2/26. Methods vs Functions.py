df46376ab8a1a31c208ebd6448d97092ba53ad31
Yaseen Amin <yaseenaminm@gmail.com>
2026-01-14 15:59:32 +0500

    27. Docstrings

--- 4. Python Basics 2/26. Methods vs Functions.py
+++ 4. Python Basics 2/26. Methods vs Functions.py
@@ -7,12 +7,9 @@
 # min()
 # input()
 
-
 # def some_random_stuff():
 #     pass
 
-
 # some_random_stuff()
-
 
 print("Hello".upper())
---
+++ 4. Python Basics 2/27. Docstrings.py
@@ -0,0 +1,14 @@
+# Docstrings
+
+def test(a):
+    '''
+    Info this function tests and prints param a
+    '''
+    print(a)
+
+
+test("!!!!")
+test("")
+print(len("Yaseen"))
+
+help(test)  # print the docstring
