ba44d31e967cad218c634bdef5c169056883bf2d
Yaseen Amin <yaseenaminm@gmail.com>
2026-01-15 15:34:53 +0500

    29. args and kwargs

--- 4. Python Basics 2/29. args and kwargs.py
+++ 4. Python Basics 2/29. args and kwargs.py
@@ -1,1 +1,18 @@
 # args and kwargs
+
+# *args and **kwargs
+
+def super_func(name, *args, i="hi", **kwargs):
+    print(*args)
+    print(kwargs)
+    # return sum(args) + sum(kwargs.values())
+    total = 0
+    for item in kwargs.values():
+        total += item
+    return sum(args) + total
+
+
+print(super_func(1, 2, 3, 4, 5, num1=5, num2=10))
+
+# Order of parameters:
+# Rules: params, *args, default parameters, **kwargs
