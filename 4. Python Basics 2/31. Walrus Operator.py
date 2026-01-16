8679b7c1b6b7151cc2503027c3f0b9e5f3360bd3
Yaseen Amin <yaseenaminm@gmail.com>
2026-01-16 17:45:58 +0500

    working on the walrus operator...

--- 4. Python Basics 2/31. Walrus Operator.py
+++ 4. Python Basics 2/31. Walrus Operator.py
@@ -4,5 +4,9 @@
 
 a = "helloooooooooooo"
 
-if (length := len(a) > 10):
+if ((length := len(a)) > 10):
     print(f"too long {length} elements")
+
+while (n := len(a) > 1):
+    print(n)
+    a = a[:-1]
