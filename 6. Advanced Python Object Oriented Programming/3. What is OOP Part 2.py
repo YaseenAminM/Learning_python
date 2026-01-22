0f74e9c899a567a8ad9973151b332c8d304ba1ae
Yaseen Amin <yaseenaminm@gmail.com>
2026-01-22 15:06:52 +0500

    3. What is OOP Part 2

---
+++ 6. Advanced Python Object Oriented Programming/3. What is OOP Part 2.py
@@ -0,0 +1,24 @@
+# What is OOP Part 2
+
+
+# Class or you can also call it  (Blueprint)
+class BigObject:
+    pass
+
+
+obj1 = BigObject()  # instanciate
+obj2 = BigObject()  # instanciate
+obj3 = BigObject()  # instanciate
+
+
+print(type(None))
+print(type(True))
+print(type(5))
+print(type(5.5))
+print(type('hi'))
+print(type([]))
+print(type(()))
+print(type({}))
+
+
+print(type(obj1))
