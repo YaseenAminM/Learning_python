9b08548d878ffb6daf5460c33bd962f793f94605
Yaseen Amin <yaseenaminm@gmail.com>
2026-02-03 15:28:21 +0500

    3. Pure Functions

--- 7. Advanced Python Functional Programming/3. Pure Functions.py
+++ 7. Advanced Python Functional Programming/3. Pure Functions.py
@@ -1,12 +1,21 @@
 # Pure Functions
 
 
-def multiply_by2(li):
-    new_list = []
-    for item in li:
-        new_list.append(item * 2)
-    return new_list
+wizard = {
+    "name": "marline",
+    "power": 50
+}
 
 
-new_list = ''
-print(multiply_by2([1, 2, 3]))
+# def multiply_by2(li):
+#     new_list = []
+#     for item in li:
+#         new_list.append(item * 2)
+#     return new_list
+
+# new_list = ''
+# print(multiply_by2([1, 2, 3]))
+
+
+def attack(character):
+    pass
