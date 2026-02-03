9b80accdf2624e9c2a7b47f88fb3042ba3478b0d
YaseenAminM <yaseenaminm@gmail.com>
2026-02-03 08:42:24 +0500

    18. super()

--- 6. Advanced Python Object Oriented Programming/18. super().py
+++ 6. Advanced Python Object Oriented Programming/18. super().py
@@ -1,5 +1,4 @@
 # super()
-
 class User(object):
     def __init__(self, email):
         self.email = email
@@ -9,7 +8,9 @@ class User(object):
 
 
 class Wizard(User):
-    def __init__(self, name, power):
+    def __init__(self, name, power, email):
+        # User.__init__(self, email)
+        super().__init__(email)
         self.name = name
         self.power = power
 
@@ -27,17 +28,6 @@ class Archer(Wizard):
         print(f"attacking with arrows: arrows left-{self.num_arrows}")
 
 
-wizard1 = Wizard("ahsan", "magic")
+wizard1 = Wizard("ahsan", "magic", "ahsan@gmail.com")
 archer1 = Archer("yaseen", "50")
-
-
-def player_attack(char):
-    char.attack()
-
-
-# player_attack(wizard1)
-# player_attack(archer1)
-
-
-for char in [wizard1, archer1]:
-    player_attack(char)
+print(wizard1.email)
