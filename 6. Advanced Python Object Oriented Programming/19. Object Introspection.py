0edfe900e35b3890d82f509540af3c88ef68edfe
YaseenAminM <yaseenaminm@gmail.com>
2026-02-03 08:46:39 +0500

    19. Object Introspection

---
+++ 6. Advanced Python Object Oriented Programming/19. Object Introspection.py
@@ -0,0 +1,36 @@
+# 19. Object Introspection
+
+
+class User(object):
+    def __init__(self, email):
+        self.email = email
+
+    def sign_in(self):
+        print("Logged In")
+
+
+class Wizard(User):
+    def __init__(self, name, power, email):
+        # User.__init__(self, email)
+        super().__init__(email)
+        self.name = name
+        self.power = power
+
+    def attack(self):
+
+        print(f"attacking with power of {self.power}")
+
+
+class Archer(Wizard):
+    def __init__(self, name, num_arrows):
+        self.name = name
+        self.num_arrows = num_arrows
+
+    def attack(self):
+        print(f"attacking with arrows: arrows left-{self.num_arrows}")
+
+
+# Introspection
+wizard1 = Wizard("ahsan", "magic", "ahsan@gmail.com")
+archer1 = Archer("yaseen", "50")
+print(dir(wizard1))
