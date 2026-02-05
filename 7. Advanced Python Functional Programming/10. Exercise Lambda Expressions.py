88b238b42cd5ad98caeb62da353f3cd124474a98
YaseenAminM <yaseenaminm@gmail.com>
2026-02-05 13:59:36 +0500

    10. Exercise Lambda Expressions

---
+++ 7. Advanced Python Functional Programming/10. Exercise Lambda Expressions.py
@@ -0,0 +1,10 @@
+# 10. Exercise Lambda Expressions
+
+# Square
+my_list = [5, 4, 3]
+print(list(map(lambda item: item**2, my_list)))
+
+a = [(0, 2), (4, 3), (10, -1), (9, 9), ]
+# List Sorting
+a.sort(key=lambda x: x[1])
+print(a)
