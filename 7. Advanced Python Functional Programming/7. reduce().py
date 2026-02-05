c1d3f28a93efc02f6f91b9aca1f3b4a8af5c1bd0
YaseenAminM <yaseenaminm@gmail.com>
2026-02-05 13:35:50 +0500

    7. reduce()

--- 7. Advanced Python Functional Programming/7. reduce().py
+++ 7. Advanced Python Functional Programming/7. reduce().py
@@ -1,1 +1,19 @@
 # reduce()
+from functools import reduce
+my_list = [1, 2, 3]
+
+
+def multiply_by_2(item):
+    return item*2
+
+
+def only_odd(item):
+    return item % 2 != 0
+
+
+def accumulator(acc, item):
+    print(acc, item)
+    return acc + item
+
+
+print(reduce(accumulator, my_list, 0))
