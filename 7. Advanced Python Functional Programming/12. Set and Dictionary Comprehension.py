4f73ebb7f18612e48c47a957b44f4c9fc9a77f86
YaseenAminM <yaseenaminm@gmail.com>
2026-02-05 22:27:29 +0500

    12. Set and Dictionary Comprehension

---
+++ 7. Advanced Python Functional Programming/12. Set and Dictionary Comprehension.py
@@ -0,0 +1,8 @@
+# Set and Dictionary Comprehension
+simple_dict = {
+    'a': 1,
+    'b': 2
+}
+# my_dict = {key: value**2 for key, value in simple_dict.items()}
+my_dict = {num: num**2 for num in [1, 2, 3]}
+print(my_dict)
