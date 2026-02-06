94032f0b9104bddca5e0dce2bcdf5db482167db4
Yaseen Amin <yaseenaminm@gmail.com>
2026-02-06 14:19:03 +0500

    1. Decorators

---
+++ 8. Advanced Python Decorators/1. Decorators.py
@@ -0,0 +1,24 @@
+# Decorators
+
+# Decorators super charge our function
+
+# @classmethod
+# @staticmethod
+
+# function in python
+
+def hello(func):
+    func()
+    print("Hello....")
+
+
+def greet():
+    print("still here!")
+
+# greet = hello
+# delete the reference
+# del hello
+# greet()
+
+# a = hello(greet)
+# print(a)
