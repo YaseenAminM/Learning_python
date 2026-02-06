9cee615ff2ba38b706fb7dd989205c18c2bf14f2
YaseenAminM <yaseenaminm@gmail.com>
2026-02-06 21:02:40 +0500

    4. Decorators 3

--- 8. Advanced Python Decorators/4. Decorators 3.py
+++ 8. Advanced Python Decorators/4. Decorators 3.py
@@ -1,16 +1,21 @@
 # Decorators 3
 
 def my_decorator(func):
-    def wrap_func(x, y):
+    def wrap_func(*args, **kwargs):
         print("**************")
         func(*args, **kwargs)
         print("**************")
     return wrap_func
 
+# This is called "dacorater pattern"
+
 
 @my_decorator
-def hello(greeting, emoji):
+def hello(greeting, emoji=':('):
     print(greeting, emoji)
 
 
-hello("hello", "😍")
+# a = my_decorator(hello)
+# a('hi')
+
+hello("hiii")
