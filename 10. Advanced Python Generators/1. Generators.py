c9634e0bcaa54863aa4f6a71cc611d546045d37b
YaseenAminM <yaseenaminm@gmail.com>
2026-02-08 01:10:14 +0500

    1. Generators

---
+++ 10. Advanced Python Generators/1. Generators.py
@@ -0,0 +1,14 @@
+# Generators
+
+# range(100)
+# print(list(range(100)))
+
+def make_list(num):
+    result = []
+    for i in range(num):
+        result.append(i*2)
+    return result
+
+
+my_list = make_list(100)
+print(my_list)
