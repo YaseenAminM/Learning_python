5a67e7b0a3821b716f9075aec66c9461c03561d0
Yaseen Amin <yaseenaminm@gmail.com>
2026-02-07 15:40:49 +0500

    1. Errors in Python

---
+++ 9. Advanced Python Error Handling/1. Errors in Python.py
@@ -0,0 +1,17 @@
+# 1. Errors in Python
+
+# Errors in Python
+common_python_errors = [
+    "SyntaxError",            # Invalid Python syntax
+    "IndentationError",       # Wrong indentation
+    "NameError",              # Variable not defined
+    "TypeError",              # Operation on wrong data type
+    "ValueError",             # Correct type, wrong value
+    "IndexError",             # List index out of range
+    "KeyError",               # Dictionary key not found
+    "AttributeError",         # Object has no such attribute
+    "ImportError",            # Error importing a module
+    "ModuleNotFoundError",    # Module does not exist
+    "ZeroDivisionError",      # Division by zero
+    "FileNotFoundError"       # File does not exist
+]
