979591c7c9d2c637c77f3e2179f63ee4324eafd1
YaseenAminM <yaseenaminm@gmail.com>
2026-02-15 17:43:08 +0500

    12. Useful Modules 2

---
+++ 11. Modules In Python/12. Useful Modules 2.py
@@ -0,0 +1,16 @@
+# 12. Useful Modules 2
+
+import datetime
+import time
+from array import array
+
+# print(date.today().year)
+# print(datetime.time(5, 59, 21))
+# print(datetime.date.today())
+
+
+# print(time.time())
+
+arr = array("L", [1, 2, 3,])
+
+print(arr[-1])
