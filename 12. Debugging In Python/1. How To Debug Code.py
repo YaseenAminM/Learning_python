b022816cc82aa2c345f1ff088e653de9619a882a
YaseenAminM <yaseenaminm@gmail.com>
2026-02-15 18:05:32 +0500

    1. How To Debug Code

---
+++ 12. Debugging In Python/1. How To Debug Code.py
@@ -0,0 +1,27 @@
+# Debugging
+
+# linting
+# num + 4
+# ide / editor
+# read "erros"
+# read docs
+# pdb - Python Debugger
+
+import pdb
+
+
+def add(num1, num2):
+    pdb.set_trace()
+    return num1 + num2
+
+
+print(add(1, 2))
+
+# psd Options in the console/Terminal
+# help
+# list
+# a
+# w
+# step
+# exit
+# next
