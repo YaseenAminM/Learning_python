ba3712080bb4e7a08f1adb5c52501f060b572b02
Yaseen Amin <yaseenaminm@gmail.com>
2026-02-16 16:24:28 +0500

    working on password regex validation...

---
+++ 14. Regular Expressions/4. Regular Expressions 3.py
@@ -0,0 +1,21 @@
+# Regular Expressions 3
+
+import re
+
+# regex for email validation
+# pattern = re.compile(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
+
+
+# string = "b@b.com"
+# string = "awdawd"
+# string = "yaseen@gmail.com"
+
+
+# regex for password validation
+
+# a = pattern.search(string)
+# b = pattern.findall(string)
+# c = pattern.fullmatch(string)
+# d = pattern.match(string)
+
+# print(a)
