bde47aefb6d7f08c4044a9d0ec749dbd79314927
Yaseen Amin <yaseenaminm@gmail.com>
2026-03-07 16:58:54 +0500

    working on the last unittest lec...

--- 15. Testing In Python/test.py
+++ 15. Testing In Python/test.py
@@ -1,49 +1,62 @@
-# Test File this file is for production
+# # Test File this file is for production
 
-# pyLint
-# pyFlakes
+# # pyLint
+# # pyFlakes
 
-# PEP 8
+# # PEP 8
+
+
+# import unittest
+# import main
+
+
+# class TestMain(unittest.TestCase):
+#     def setUp(self):
+#         print("about to test function")
+
+#     def test_do_stuff(self):
+#         test_param = 10
+#         result = main.do_stuff(test_param)
+#         self.assertEqual(result, 15)
+
+#     def test_do_stuff2(self):
+#         """
+#         TEST 1
+#         """
+#         test_param = "adadwwa"
+#         result = main.do_stuff(test_param)
+#         self.assertIsInstance(result, ValueError)
+
+#     def test_do_stuff3(self):
+#         test_param = None
+#         result = main.do_stuff(test_param)
+#         self.assertEqual(result, "please enter number")
+
+#     def test_do_stuff4(self):
+#         test_param = ''
+#         result = main.do_stuff(test_param)
+#         self.assertEqual(result, "please enter number")
+
+#     def test_do_stuff5(self):
+#         test_param = 'awdawdawd'
+#         result = main.do_stuff(test_param)
+#         self.assertTrue(isinstance(result, Exception))
+
+#     def tearDown(self):
+#         print("cleaning up")
+
+
+# if __name__ == '__main__':
+#     unittest.main()
 
 
 import unittest
-import main
+import exercise
 
 
-class TestMain(unittest.TestCase):
-    def setUp(self):
-        print("about to test function")
-
-    def test_do_stuff(self):
-        test_param = 10
-        result = main.do_stuff(test_param)
-        self.assertEqual(result, 15)
-
-    def test_do_stuff2(self):
-        """ 
-        TEST 1
-        """
-        test_param = "adadwwa"
-        result = main.do_stuff(test_param)
-        self.assertIsInstance(result, ValueError)
-
-    def test_do_stuff3(self):
-        test_param = None
-        result = main.do_stuff(test_param)
-        self.assertEqual(result, "please enter number")
-
-    def test_do_stuff4(self):
-        test_param = ''
-        result = main.do_stuff(test_param)
-        self.assertEqual(result, "please enter number")
-
-    def test_do_stuff5(self):
-        test_param = 'awdawdawd'
-        result = main.do_stuff(test_param)
-        self.assertTrue(isinstance(result, Exception))
-
-    def tearDown(self):
-        print("cleaning up")
+class TestScript(unittest.TestCase):
+    def test_input(self):
+        pass
 
 
 if __name__ == '__main__':
