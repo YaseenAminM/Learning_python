06a48a86fb8344a35bc903ab35688c9728f2cab1
Yaseen Amin <yaseenaminm@gmail.com>
2026-03-07 14:26:12 +0500

    4. Writing Unit Tests 2

--- 15. Testing In Python/test2.py
+++ 15. Testing In Python/test2.py
@@ -11,18 +11,12 @@ import main
 
 
 class TestMain(unittest.TestCase):
-    def setUp(self):
-        print("about to test function")
-
     def test_do_stuff(self):
         test_param = 10
         result = main.do_stuff(test_param)
         self.assertEqual(result, 15)
 
     def test_do_stuff2(self):
-        """ 
-        TEST 1
-        """
         test_param = "adadwwa"
         result = main.do_stuff(test_param)
         self.assertIsInstance(result, ValueError)
@@ -41,9 +35,6 @@ class TestMain(unittest.TestCase):
         test_param = 'awdawdawd'
         result = main.do_stuff(test_param)
         self.assertTrue(isinstance(result, Exception))
-
-    def tearDown(self):
-        print("cleaning up")
 
 
 if __name__ == '__main__':
