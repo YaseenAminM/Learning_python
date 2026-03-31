e45016c9b4a51a038edb4819797c2117a810bf88
Yaseen Amin <yaseenaminm@gmail.com>
2026-03-31 16:00:02 +0500

    14. Exercise PDF Merger

--- 17. Scripting With Python/pdfs/pdf.py
+++ 17. Scripting With Python/pdfs/pdf.py
@@ -1,14 +1,32 @@
 import PyPDF2
 
-with open("dummy.pdf", 'rb') as file:
-    reader = PyPDF2.PdfReader(file)
-    print(len(reader.pages))
+# with open("dummy.pdf", 'rb') as file:
+#     reader = PyPDF2.PdfReader(file)
+#     print(len(reader.pages))
 
-    page = reader.pages[0]
-    rotate = page.rotate(90)
-    print(rotate)
+#     page = reader.pages[0]
+#     rotate = page.rotate(90)
+#     print(rotate)
 
-    write = PyPDF2.PdfWriter()        # ✅ was PdfFileWriter
-    write.add_page(rotate)
-    with open("tilt.pdf", "wb") as new_file:
-        write.write(new_file)
+#     write = PyPDF2.PdfWriter()        # ✅ was PdfFileWriter
+#     write.add_page(rotate)
+#     with open("tilt.pdf", "wb") as new_file:
+#         write.write(new_file)
+
+
+# ===================================================
+with open("dummy.pdf", "rb") as f1, open("tilt.pdf", "rb") as f2:
+    reader1 = PyPDF2.PdfReader(f1)
+    reader2 = PyPDF2.PdfReader(f2)
+
+    writer = PyPDF2.PdfWriter()
+
+    # Add all pages from both files
+    for page in reader1.pages:
+        writer.add_page(page)
+
+    for page in reader2.pages:
+        writer.add_page(page)
+
+    with open("combined.pdf", "wb") as output:
+        writer.write(output)
