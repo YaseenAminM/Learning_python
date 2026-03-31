b18ad029efb6ac45e50b7a5580ad9b59e791b568
Yaseen Amin <yaseenaminm@gmail.com>
2026-03-31 15:46:22 +0500

    13. PDFs With Python

--- 17. Scripting With Python/pdfs/pdf.py
+++ 17. Scripting With Python/pdfs/pdf.py
@@ -3,6 +3,12 @@ import PyPDF2
 with open("dummy.pdf", 'rb') as file:
     reader = PyPDF2.PdfReader(file)
     print(len(reader.pages))
-    print(reader.pages[0])
 
-    # page = reader.pages[0]
+    page = reader.pages[0]
+    rotate = page.rotate(90)
+    print(rotate)
+
+    write = PyPDF2.PdfWriter()        # ✅ was PdfFileWriter
+    write.add_page(rotate)
+    with open("tilt.pdf", "wb") as new_file:
+        write.write(new_file)
