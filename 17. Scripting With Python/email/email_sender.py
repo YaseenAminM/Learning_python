8f645decdb9aa56cc87a18e3a7c234a62c8b4dcc
Yaseen Amin <yaseenaminm@gmail.com>
2026-04-07 15:52:30 +0500

    20. Sending Emails With Python 3

--- 17. Scripting With Python/email/email_sender.py
+++ 17. Scripting With Python/email/email_sender.py
@@ -1,18 +1,26 @@
 import smtplib
 from email.message import EmailMessage
+from string import Template
+from pathlib import Path
+
+
+html = Template(Path("index.html").read_text())
+
 
 email = EmailMessage()
-email['from'] = "yaseen"
-email['to'] = "test@gmail.com"
-email['subject'] = "You won 1,000,000 dollars..."
+email['from'] = "from_mail@gmail.com"
+email['to'] = "to_mail@gmail.com"
+email['subject'] = "<E-mail-subject>..."
 
-email.set_content("I am a Python Master 🔥")
+# email.set_content(html.substitute(name="YaseenAmin"))
+
+email.set_content(html.substitute({"name": "YaseenAmin"}), "html")
 
 try:
     with smtplib.SMTP(host="smtp.gmail.com", port=587) as smtp:
         smtp.ehlo()
         smtp.starttls()
-        smtp.login("test@gmail.com", "123456789")
+        smtp.login("from_mail@gmail.com", "gooale-app-password")
         smtp.send_message(email)
         print("✅ Email sent successfully!")
 
