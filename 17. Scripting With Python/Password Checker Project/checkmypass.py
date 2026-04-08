1cabc8674de57f52cc8960e252c43fa2fbe22468
Yaseen Amin <yaseenaminm@gmail.com>
2026-04-08 15:59:47 +0500

    27. Password Checker 2

--- 17. Scripting With Python/Password Checker Project/checkmypass.py
+++ 17. Scripting With Python/Password Checker Project/checkmypass.py
@@ -5,7 +5,7 @@ import hashlib
 def request_api_data(query_char):
     url = 'https://api.pwnedpasswords.com/range/' + query_char
     res = requests.get(url)
-    print(res)
+    # print(res)
 
     if res.status_code != 200:
         raise RuntimeError(
@@ -13,10 +13,28 @@ def request_api_data(query_char):
     return res
 
 
+def get_password_leaks_count(hashes, hash_to_check):
+    hashes = (line.split(":") for line in hashes.text.splitlines())
+    print(hashes)
+
+    for h, count in hashes:
+        print(h, count)
+
+
+def read_response(response):
+    print(response.text)
+
+
 def pwned_api_check(password):
     sha1password = hashlib.sha1(
         password.encode("utf-8")).hexdigest().upper()
-    return sha1password
+    first5_char, tail = sha1password[:5], sha1password[5:]
+
+    response = request_api_data(first5_char)
+    print(
+        f"Full Password : {sha1password}\nfirst_5_char : {first5_char}\nTail: {tail}")
+    get_password_leaks_count(response, tail)
+    return response
 
 
 print(pwned_api_check("password123"))
